7d69e1559d20fcb4e8e1d63a343129f8d991bba7
Pedro Paulo <pedro.vvs@outlook.com>
2023-01-20 14:43:23 -0300

    Upload project Render

--- dist/app/__mocks__/user.mock.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    login: '',
-    password: '',
-    token: '',
-};
-//# sourceMappingURL=user.mock.js.map
